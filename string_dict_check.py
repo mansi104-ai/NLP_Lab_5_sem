04c1eb771d4324325332cda2094f80d4493bd8a8
mansi104-ai <118623172+mansi104-ai@users.noreply.github.com>
2024-10-03 10:42:43 +0530

    dictionary spell checker added

---
+++ string_dict_check.py
@@ -0,0 +1,10 @@
+dictionary = {"apple","banana","text","token"}
+
+#Sample input
+input_string = "apple"
+
+#Check if the input string is present in the dictionary
+if input_string in dictionary:
+    print(f"'{input_string}' is in the dictionary.")
+else:
+    print(f"'{input_string}' is not in the dictionary")
